c35796f70d3400d8a9f1db1b215f7e02cea4c3dd
Fraser Wilson <blancolioni@gmail.com>
2016-03-27 13:25:23 +0200

    Galaxy.Updates: system changes hands if occupied by exactly one empire which is at war with old owner

--- src/galaxy/concorde-galaxy-updates.adb
+++ src/galaxy/concorde-galaxy-updates.adb
@@ -6,6 +6,7 @@ with Concorde.Galaxy.Ships;
 with Concorde.AI;
 with Concorde.Empires;
 with Concorde.Empires.Logging;
+with Concorde.Empires.Relations;
 
 with Concorde.Ships.Battles;
 with Concorde.Ships.Lists;
@@ -26,43 +27,70 @@ package body Concorde.Galaxy.Updates is
             List : Concorde.Ships.Lists.List;
          begin
             System.Get_Ships (List);
-            if not List.Is_Empty
-              and then not Concorde.Ships.Battles.Has_Conflict (List)
-            then
+            if not List.Is_Empty then
                declare
-                  use Concorde.Empires;
-                  Current_Owner : constant Empire_Type :=
-                                    Empire_Type (System.Owner);
-                  Ship_Owner    : constant Empire_Type :=
-                                    Empire_Type (List.First_Element.Owner);
+                  Es : constant Concorde.Empires.Array_Of_Empires :=
+                         Concorde.Ships.Battles.Empires_Present (List);
                begin
-                  if Current_Owner /= Ship_Owner then
-                     Changed_Owners := True;
-                     if Current_Owner /= null then
-                        Current_Owner.System_Lost
-                          (Concorde.Systems.Star_System_Type (System));
-                        Current_Owner.AI.System_Lost
-                          (Concorde.Systems.Star_System_Type (System),
-                           Ship_Owner);
-                        Concorde.Empires.Logging.Log
-                          (Current_Owner,
-                           "loses control of " & System.Name & " to "
-                           & Ship_Owner.Name);
-                        Concorde.Empires.Logging.Log
-                          (Ship_Owner,
-                           "acquires " & System.Name & " from "
-                           & Current_Owner.Name);
-                     else
-                        Concorde.Empires.Logging.Log
-                          (Ship_Owner,
-                           "acquires " & System.Name);
+                  if not System.Owned then
+                     if Es'Length = 1 then
+                        --  colonise unclaimed world
+                        declare
+                           New_Owner : constant Concorde.Empires.Empire_Type :=
+                                         Es (Es'First);
+                        begin
+                           Changed_Owners := True;
+                           Concorde.Empires.Logging.Log
+                             (New_Owner,
+                              "colonises " & System.Name);
+                           System.Set_Owner (New_Owner);
+                           New_Owner.System_Acquired
+                             (Concorde.Systems.Star_System_Type (System));
+                           New_Owner.AI.System_Acquired
+                             (Concorde.Systems.Star_System_Type (System),
+                              null);
+                        end;
                      end if;
-                     System.Set_Owner (Ship_Owner);
-                     Ship_Owner.System_Acquired
-                       (Concorde.Systems.Star_System_Type (System));
-                     Ship_Owner.AI.System_Acquired
-                       (Concorde.Systems.Star_System_Type (System),
-                        Current_Owner);
+                  elsif not Concorde.Empires.Relations.Has_Conflict (Es) then
+                     declare
+                        use Concorde.Empires, Concorde.Empires.Relations;
+                        Current_Owner : constant Empire_Type := System.Owner;
+                        New_Owner     : Empire_Type := null;
+                     begin
+                        for E of Es loop
+                           if At_War (E, Current_Owner) then
+                              if New_Owner = null then
+                                 New_Owner := E;
+                              else
+                                 New_Owner := null;
+                                 exit;
+                              end if;
+                           end if;
+                        end loop;
+
+                        if New_Owner /= null then
+                           Changed_Owners := True;
+                           Current_Owner.System_Lost
+                             (Concorde.Systems.Star_System_Type (System));
+                           Current_Owner.AI.System_Lost
+                             (Concorde.Systems.Star_System_Type (System),
+                              New_Owner);
+                           New_Owner.System_Acquired
+                             (Concorde.Systems.Star_System_Type (System));
+                           New_Owner.AI.System_Acquired
+                             (Concorde.Systems.Star_System_Type (System),
+                              Current_Owner);
+                           System.Set_Owner (New_Owner);
+                           Concorde.Empires.Logging.Log
+                             (Current_Owner,
+                              "loses control of " & System.Name & " to "
+                              & New_Owner.Name);
+                           Concorde.Empires.Logging.Log
+                             (New_Owner,
+                              "acquires " & System.Name & " from "
+                              & Current_Owner.Name);
+                        end if;
+                     end;
                   end if;
                end;
             end if;
