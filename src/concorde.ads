d9c40adfa9b5ab168330a7de8486e2b99581c64a
Fraser Wilson <blancolioni@gmail.com>
2016-04-13 12:38:05 +0200

    Removed Radians type from Concorde package; it's now a private type in Concorde.Geometry.

--- src/concorde.ads
+++ src/concorde.ads
@@ -1,4 +1,3 @@
-with Ada.Numerics;
 with Lui;
 
 package Concorde is
@@ -8,8 +7,6 @@ package Concorde is
    subtype Unit_Real is Real range 0.0 .. 1.0;
 
    subtype Non_Negative_Real is Real range 0.0 .. Real'Last;
-
-   subtype Radians is Real range 0.0 .. 2.0 * Ada.Numerics.Pi;
 
    type Point_Type is
       record
