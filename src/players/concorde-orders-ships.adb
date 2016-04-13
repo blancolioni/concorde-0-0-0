c8e01be66603b9b5ba191d6e0f59509ee628a5a9
Fraser Wilson <blancolioni@gmail.com>
2016-04-13 22:35:19 +0200

    Added ship colonise order

--- src/players/concorde-orders-ships.adb
+++ src/players/concorde-orders-ships.adb
@@ -2,16 +2,48 @@ with Concorde.Ships.Db;
 
 package body Concorde.Orders.Ships is
 
-   type Move_Ship_Order_Record is
-     new Root_Order_Type with
+   type Root_Ship_Order_Record is
+     abstract new Root_Order_Type with
       record
          Empire      : Concorde.Empires.Empire_Type;
          Ship        : Concorde.Ships.Ship_Type;
+      end record;
+
+   type Move_Ship_Order_Record is
+     new Root_Ship_Order_Record with
+      record
          Destination : Concorde.Systems.Star_System_Type;
       end record;
 
    overriding procedure Execute
      (Order : Move_Ship_Order_Record);
+
+   type Colonise_Order_Record is
+     new Root_Ship_Order_Record with
+      record
+         null;
+      end record;
+
+   overriding procedure Execute
+     (Order : Colonise_Order_Record);
+
+   --------------------
+   -- Colonise_Order --
+   --------------------
+
+   function Colonise_Order
+     (Empire      : not null access constant
+        Concorde.Empires.Root_Empire_Type'Class;
+      Ship        : not null access constant
+        Concorde.Ships.Root_Ship_Type'Class)
+      return Root_Order_Type'Class
+   is
+   begin
+      return Result : Colonise_Order_Record do
+         Result.Empire := Concorde.Empires.Empire_Type (Empire);
+         Result.Ship := Concorde.Ships.Ship_Type (Ship);
+      end return;
+   end Colonise_Order;
 
    -------------
    -- Execute --
@@ -43,6 +75,20 @@ package body Concorde.Orders.Ships is
          Concorde.Ships.Db.Update
            (Order.Ship.Reference, Set_Destination'Access);
       end if;
+   end Execute;
+
+   -------------
+   -- Execute --
+   -------------
+
+   overriding procedure Execute
+     (Order : Colonise_Order_Record)
+   is
+      pragma Unreferenced (Order);
+   begin
+      --  this happens automatically; the order is just to force
+      --  the ship to do nothing
+      null;
    end Execute;
 
    ---------------------
--- src/players/concorde-orders-ships.ads
+++ src/players/concorde-orders-ships.ads
@@ -11,6 +11,15 @@ package Concorde.Orders.Ships is
         Concorde.Ships.Root_Ship_Type'Class;
       Destination : not null access constant
         Concorde.Systems.Root_Star_System_Type'Class)
-      return Root_Order_Type'Class;
+      return Root_Order_Type'Class
+     with Pre => Ship.System.Index /= Destination.Index;
+
+   function Colonise_Order
+     (Empire      : not null access constant
+        Concorde.Empires.Root_Empire_Type'Class;
+      Ship        : not null access constant
+        Concorde.Ships.Root_Ship_Type'Class)
+      return Root_Order_Type'Class
+     with Pre => not Ship.System.Owned;
 
 end Concorde.Orders.Ships;
