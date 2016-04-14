cfb56c35f36b395b131aabfd1039366158906b11
Fraser Wilson <blancolioni@gmail.com>
2016-04-14 17:04:16 +0200

    Skills db package and initial config file

---
+++ src/db/concorde-people-skills-db.ads
@@ -0,0 +1,7 @@
+with Memor.Database;
+
+private package Concorde.People.Skills.Db is
+  new Memor.Database
+    (Class_Name        => "pop-skill",
+     Element_Type      => Root_Pop_Skill,
+     Element_Reference => Pop_Skill);
