137e85d648c03123c77fbdc22a603fa1c92ab9b4
Fraser Wilson <blancolioni@gmail.com>
2016-04-24 21:57:12 +0200

    Ships.Updates: checking for completed buy and sell orders, and setting next destination

--- src/ships/concorde-ships-updates.adb
+++ src/ships/concorde-ships-updates.adb
@@ -11,6 +11,9 @@ with Concorde.Systems.Db;
 
 package body Concorde.Ships.Updates is
 
+   function Is_Alive (Ship : Root_Ship_Type'Class) return Boolean
+   is (Ship.Alive);
+
    package List_Of_References is
      new Ada.Containers.Doubly_Linked_Lists
        (Memor.Database_Reference, Memor."=");
@@ -97,10 +100,90 @@ package body Concorde.Ships.Updates is
    ------------------
 
    procedure Update_Ship_Movement is
-      function Is_Alive (Ship : Root_Ship_Type'Class) return Boolean
-      is (Ship.Alive);
    begin
       Concorde.Ships.Db.Iterate (Is_Alive'Access, Update_Ship'Access);
    end Update_Ship_Movement;
+
+   ------------------------
+   -- Update_Ship_Orders --
+   ------------------------
+
+   procedure Update_Ship_Orders is
+
+      function Alive_At_Destination
+        (Ship : Root_Ship_Type'Class)
+         return Boolean
+      is (Ship.Alive and then not Ship.Has_Destination);
+
+      procedure Update (Ship : in out Root_Ship_Type'Class);
+
+      ------------
+      -- Update --
+      ------------
+
+      procedure Update (Ship : in out Root_Ship_Type'Class) is
+         use List_Of_Orders;
+         Cycle_List : List;
+         Position : Cursor := Ship.Orders.First;
+      begin
+         while Has_Element (Position) loop
+            declare
+               use type Memor.Database_Reference;
+               use type Concorde.Quantities.Quantity;
+               Order : constant Ship_Order_Record := Element (Position);
+            begin
+
+               if Order.System_Reference = Ship.System_Reference then
+                  case Order.Order is
+                     when No_Order =>
+                        Ship.Orders.Delete (Position);
+                        Position := Ship.Orders.First;
+                     when Buy =>
+                        if Ship.Total_Quantity = Ship.Hold_Quantity then
+                           Ship.Log_Trade ("Finished buying "
+                                           & Order.Commodity.Name);
+                           if Ship.Cycle_Orders then
+                              Cycle_List.Append (Order);
+                           end if;
+                           Ship.Orders.Delete (Position);
+                           Position := Ship.Orders.First;
+                        else
+                           exit;
+                        end if;
+                     when Sell =>
+                        if Ship.Get_Quantity (Order.Commodity)
+                          = Quantities.Zero
+                        then
+                           Ship.Log_Trade ("Finished selling "
+                                           & Order.Commodity.Name);
+                           if Ship.Cycle_Orders then
+                              Cycle_List.Append (Order);
+                           end if;
+                           Ship.Orders.Delete (Position);
+                           Position := Ship.Orders.First;
+                        else
+                           exit;
+                        end if;
+                  end case;
+               else
+                  Ship.Log_Trade
+                    ("Finished trading; heading to "
+                     & Concorde.Systems.Db.Element
+                       (Order.System_Reference).Name);
+                  Ship.Dest_Reference := Order.System_Reference;
+                  exit;
+               end if;
+            end;
+         end loop;
+
+         for Order of Cycle_List loop
+            Ship.Orders.Append (Order);
+         end loop;
+
+      end Update;
+
+   begin
+      Concorde.Ships.Db.Iterate (Alive_At_Destination'Access, Update'Access);
+   end Update_Ship_Orders;
 
 end Concorde.Ships.Updates;
--- src/ships/concorde-ships-updates.ads
+++ src/ships/concorde-ships-updates.ads
@@ -1,6 +1,7 @@
 package Concorde.Ships.Updates is
 
    procedure Update_Ship_Movement;
+   procedure Update_Ship_Orders;
 
    procedure Delete_Dead_Ships;
 
