48d00d761be4993b173b21049c2212e979e394cf
Fraser Wilson <blancolioni@gmail.com>
2016-08-22 16:42:43 +0200

    Added prototype Xi Ship model

---
+++ src/ui/xi/concorde-ships-xi_model.ads
@@ -0,0 +1,9 @@
+with Concorde.Xi_UI;
+
+package Concorde.Ships.Xi_Model is
+
+   procedure Transit_To_Ship
+     (Ship   : Ship_Type;
+      Model  : in out Concorde.Xi_UI.Root_Xi_Model'Class);
+
+end Concorde.Ships.Xi_Model;
