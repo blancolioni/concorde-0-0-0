e9f01f20fda9c615afade3fd64002fb20dc8e115
Fraser Wilson <blancolioni@gmail.com>
2016-09-06 10:17:50 +0200

    Concorde.Dates: now including time as well as date.  Date_Type now private.  Added function "-" for two dates, returning a duration.

--- src/driver/concorde-dates.ads
+++ src/driver/concorde-dates.ads
@@ -1,12 +1,28 @@
 package Concorde.Dates is
 
-   type Date_Type is new Natural;
+   type Date_Type is private;
+
+   Zero_Date : constant Date_Type;
 
    function Current_Date return Date_Type;
    function Current_Date_To_String return String;
 
-   procedure Tick;
+   procedure Tick (Simulation_Seconds : Duration);
 
    function To_String (Date : Date_Type) return String;
+
+   function To_Date_And_Time_String (Date : Date_Type) return String;
+
+   function "-" (Left, Right : Date_Type) return Duration;
+
+   type Day_Index is new Positive;
+
+   function Get_Day (Date : Date_Type) return Day_Index;
+
+private
+
+   type Date_Type is new Long_Float;
+
+   Zero_Date : constant Date_Type := 0.0;
 
 end Concorde.Dates;
