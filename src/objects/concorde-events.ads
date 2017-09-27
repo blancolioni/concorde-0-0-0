894232e60adb02023400a48d793519f9a7226dad
Fraser Wilson <blancolioni@gmail.com>
2017-09-27 10:58:39 +0200

    Concorde.Events: events now have a time stamp

--- src/objects/concorde-events.ads
+++ src/objects/concorde-events.ads
@@ -1,16 +1,34 @@
+with Concorde.Dates;
+
 package Concorde.Events is
 
    type Root_Event_Type is abstract tagged private;
 
-   function Null_Event return Root_Event_Type'Class;
+   function Time_Stamp
+     (Event : Root_Event_Type'Class)
+      return Concorde.Dates.Date_Type;
+
+   function Null_Event
+     (Time_Stamp : Concorde.Dates.Date_Type)
+     return Root_Event_Type'Class;
 
 private
 
-   type Root_Event_Type is abstract tagged null record;
+   type Root_Event_Type is abstract tagged
+      record
+         Time_Stamp : Concorde.Dates.Date_Type;
+      end record;
+
+   function Time_Stamp
+     (Event : Root_Event_Type'Class)
+      return Concorde.Dates.Date_Type
+   is (Event.Time_Stamp);
 
    type Null_Event_Type is new Root_Event_Type with null record;
 
-   function Null_Event return Root_Event_Type'Class
-   is (Null_Event_Type'(null record));
+   function Null_Event
+     (Time_Stamp : Concorde.Dates.Date_Type)
+      return Root_Event_Type'Class
+   is (Null_Event_Type'(Time_Stamp => Time_Stamp));
 
 end Concorde.Events;
