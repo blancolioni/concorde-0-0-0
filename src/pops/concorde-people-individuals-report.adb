1d59798c4cd52e4e291c36e0d7d36a5f0589a140
Fraser Wilson <blancolioni@gmail.com>
2018-01-12 09:46:56 +0100

    New package Concorde.People.Individuals.Reports.  Write reports to individuals.txt in current directory.

---
+++ src/pops/concorde-people-individuals-report.adb
@@ -0,0 +1,39 @@
+with Ada.Text_IO;
+
+package body Concorde.People.Individuals.Report is
+
+   First : Boolean := True;
+
+   ------------
+   -- Report --
+   ------------
+
+   procedure Report
+     (Individual : not null access constant Root_Individual_Type'Class)
+   is
+      use Ada.Text_IO;
+      File : File_Type;
+   begin
+      if First then
+         Create (File, Out_File, "individuals.txt");
+         Close (File);
+         First := False;
+      end if;
+
+      Open (File, Append_File, "individuals.txt");
+      Set_Output (File);
+      Put_Line (Individual.Full_Name & " born "
+                & Concorde.Calendar.Image (Individual.Birth)
+                & " (age" & Natural'Image (Individual.Age) & ")");
+      for Ability in Individual.Scores'Range loop
+         Put (Ability_Type'Image (Ability) & ":");
+         Set_Col (16);
+         Put (Individual.Scores (Ability)'Img);
+         New_Line;
+      end loop;
+      New_Line;
+      Set_Output (Standard_Output);
+      Close (File);
+   end Report;
+
+end Concorde.People.Individuals.Report;
---
+++ src/pops/concorde-people-individuals-report.ads
@@ -0,0 +1,6 @@
+package Concorde.People.Individuals.Report is
+
+   procedure Report
+     (Individual : not null access constant Root_Individual_Type'Class);
+
+end Concorde.People.Individuals.Report;
