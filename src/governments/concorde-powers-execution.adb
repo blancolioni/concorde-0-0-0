f7a6f4e58d8b667654ca0770fbb10ee0675c15a2
Fraser Wilson <blancolioni@gmail.com>
2018-01-31 17:45:10 +0100

    New package Concorde.Powers.Execution;

---
+++ src/governments/concorde-powers-execution.adb
@@ -0,0 +1,151 @@
+with WL.Quantities;
+
+with Concorde.Calendar;
+with Concorde.Commodities;
+with Concorde.People.Skills;
+
+package body Concorde.Powers.Execution is
+
+   ---------------
+   -- Attribute --
+   ---------------
+
+   function Attribute
+     (Power : Power_Type;
+      Index : Positive)
+      return Concorde.People.Attributes.Attribute_Reference
+   is
+      pragma Unreferenced (Index);
+   begin
+      return Concorde.People.Attributes.Skill_Reference
+        (Concorde.People.Skills.Get ("administration"));
+   end Attribute;
+
+   ---------------------
+   -- Attribute_Count --
+   ---------------------
+
+   function Attribute_Count
+     (Power : Power_Type)
+      return Natural
+   is
+      pragma Unreferenced (Power);
+   begin
+      return 1;
+   end Attribute_Count;
+
+   ----------------
+   -- Daily_Work --
+   ----------------
+
+   function Daily_Work
+     (Power : Power_Type;
+      World : Concorde.Worlds.World_Type)
+      return Duration
+   is
+   begin
+      case Power.Class is
+         when Set_Tax_Rate =>
+            return Concorde.Calendar.Hours (8);
+
+         when Collect_Tax =>
+            declare
+               Work : Duration :=
+                        Concorde.Calendar.Hours (8);
+
+               procedure Add_Commodity
+                 (Commodity : Concorde.Commodities.Commodity_Type);
+
+               -------------------
+               -- Add_Commodity --
+               -------------------
+
+               procedure Add_Commodity
+                 (Commodity : Concorde.Commodities.Commodity_Type)
+               is
+                  Result : Duration := 0.0;
+               begin
+                  case Power.Tax_Category is
+                     when Concorde.Trades.Sales =>
+                        Result :=
+                          Duration
+                            (World.Market.Recent_Transaction_Count
+                               (Commodity));
+                     when Concorde.Trades.Import =>
+                        Result :=
+                          Duration
+                            (WL.Quantities.To_Float
+                               (World.Market.Current_Imports (Commodity))
+                             / 1_000.0);
+                     when Concorde.Trades.Export =>
+                        Result :=
+                          Duration
+                            (WL.Quantities.To_Float
+                               (World.Market.Current_Exports (Commodity))
+                             / 1_000.0);
+                  end case;
+                  if Result > 0.0 then
+                     Result := Result + Concorde.Calendar.Hours (1);
+                  end if;
+                  Work := Work + Result;
+               end Add_Commodity;
+
+            begin
+               Concorde.Commodities.Scan (Add_Commodity'Access);
+               return Work;
+            end;
+
+      end case;
+   end Daily_Work;
+
+   ---------------
+   -- Pop_Group --
+   ---------------
+
+   function Pop_Group
+     (Power : Power_Type;
+      Index : Positive)
+      return Concorde.People.Groups.Pop_Group
+   is
+      pragma Unreferenced (Index);
+   begin
+      case Power.Class is
+         when Set_Tax_Rate =>
+            return Concorde.People.Groups.Get ("clerks");
+         when Collect_Tax =>
+            return Concorde.People.Groups.Get ("bureaucrats");
+      end case;
+   end Pop_Group;
+
+   ---------------------
+   -- Pop_Group_Count --
+   ---------------------
+
+   function Pop_Group_Count
+     (Power : Power_Type)
+      return Natural
+   is
+   begin
+      case Power.Class is
+         when Set_Tax_Rate =>
+            return 1;
+         when Collect_Tax =>
+            return 1;
+      end case;
+   end Pop_Group_Count;
+
+   ----------------------
+   -- Pop_Group_Effect --
+   ----------------------
+
+   function Pop_Group_Effect
+     (Power : Power_Type;
+      Index : Positive)
+      return Duration
+   is
+      pragma Unreferenced (Index);
+   begin
+      return Concorde.Calendar.Hours (4);
+   end Pop_Group_Effect;
+
+end Concorde.Powers.Execution;
---
+++ src/governments/concorde-powers-execution.ads
@@ -0,0 +1,51 @@
+with Concorde.People.Attributes;
+with Concorde.People.Groups;
+with Concorde.Worlds;
+
+package Concorde.Powers.Execution is
+
+   function Daily_Work
+     (Power : Power_Type;
+      World : Concorde.Worlds.World_Type)
+      return Duration;
+   --  How many person-seconds required
+   --  to execute this power on the given world today
+
+   function Pop_Group_Count
+     (Power : Power_Type)
+      return Natural;
+   --  Number of pop group categories that can execute this power
+
+   function Pop_Group
+     (Power : Power_Type;
+      Index : Positive)
+      return Concorde.People.Groups.Pop_Group
+     with Pre => Pop_Group_Count (Power) >= Index,
+     Post => Concorde.People.Groups."/=" (Pop_Group'Result, null);
+
+   function Pop_Group_Effect
+     (Power : Power_Type;
+      Index : Positive)
+      return Duration
+     with Pre => Pop_Group_Count (Power) >= Index;
+   --  Number of seconds per day contributed by each
+   --  person in this pop group
+
+   function Attribute_Count
+     (Power : Power_Type)
+      return Natural;
+
+   function Attribute
+     (Power : Power_Type;
+      Index : Positive)
+      return Concorde.People.Attributes.Attribute_Reference
+     with Pre => Pop_Group_Count (Power) >= Index;
+   --  Attributes reduce the daily work required to execute this power
+   --  They are contributed by individuals executing the power.
+   --  Effect is reduced for each degree of separation from execution.
+   --  E.g. an individual directly executing a power gets full effect
+   --  Individual who manages a ministry which executes the power gets half
+   --  That individual's boss gets less, depending on how much time they
+   --  can contribute
+
+end Concorde.Powers.Execution;
